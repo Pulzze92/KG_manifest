19da0a95e586888fc0d34f86ea8b8f2dca8abf7b
Pulzze92 <76836870+Pulzze92@users.noreply.github.com>
2024-12-02 16:37:15 +0100

    working hook with send transaction in ton

--- useTonConnect.ts
+++ useTonConnect.ts
@@ -5,9 +5,8 @@ import {
   useTonAddress,
   useTonConnectUI,
 } from '@tonconnect/ui-react';
-import { beginCell, toNano, Address } from 'ton-core';
 
-const API_URL = 'https://tonapi.io';
+const API_URL = 'https://tonapi.io/';
 
 export const useTonConnect = () => {
   const { state, open, close } = useTonConnectModal();
@@ -48,76 +47,6 @@ export const useTonConnect = () => {
     disconnectFromStore();
   };
 
-  const getJettonWalletAddress = async (
-    userWallet: string,
-    tokenMaster: string
-  ) => {
-    try {
-      const url = `${API_URL}/v2/blockchain/accounts/${tokenMaster}/methods/get_wallet_address?args=${userWallet}`;
-      const response = await fetch(url);
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch Jetton Wallet address');
-      }
-
-      const data = await response.json();
-      console.log('Fetched Jetton wallet address:', data);
-      return data.decoded.jetton_wallet_address;
-    } catch (error) {
-      console.error('Failed to get Jetton Wallet Address:', error);
-      throw error;
-    }
-  };
-
-  const createJettonTransferMessage = async (
-    userWallet: string,
-    amount: string,
-    collectorAddress: string
-  ) => {
-    try {
-      // const parsedUserWallet = Address.parse(userWallet);
-      const parsedCollectorAddress = Address.parse(collectorAddress);
-
-      const walletAddress = await getJettonWalletAddress(
-        userWallet,
-        collectorAddress
-      );
-      console.log('Fetched wallet address:', walletAddress);
-
-      if (!walletAddress) {
-        throw new Error('Failed to retrieve Jetton Wallet address.');
-      }
-
-      const forwardPayload = beginCell()
-        .storeUint(0, 32) // 0 opcode means we have a comment
-        .storeStringTail('Hello, TON!')
-        .endCell();
-
-      const cell = beginCell()
-        .storeUint(0xf8a7ea5, 32)
-        .storeUint(0, 64)
-        .storeCoins(toNano(5))
-        .storeAddress(parsedCollectorAddress)
-        .storeAddress(parsedCollectorAddress)
-        .storeBit(0)
-        .storeCoins(toNano('0.02'))
-        .storeBit(1)
-        .storeRef(forwardPayload)
-        .endCell();
-
-      const boc = cell.toBoc().toString('base64');
-
-      return {
-        address: walletAddress,
-        payload: boc,
-        amount: '1',
-      };
-    } catch (error) {
-      console.error('Failed to create Jetton transfer message:', error);
-      throw error;
-    }
-  };
-
   const fetchTokenBalance = async (address: string, jettonId: string) => {
     try {
       const response = await fetch(
@@ -131,42 +60,92 @@ export const useTonConnect = () => {
     }
   };
 
+  const sendTokenTransaction = async (
+    toAddress: string,
+    tokenAddress: string,
+    amount: string
+  ) => {
+    try {
+      const response = await fetch(`${API_URL}/address/${toAddress}/send`, {
+        method: 'POST',
+        body: JSON.stringify({
+          token: tokenAddress,
+          amount: amount,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to send token');
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Transaction failed:', error);
+      throw error;
+    }
+  };
+
   const sendTransaction = async (
     toAddress: string,
     amount: string,
-    collectorAddress: string
+    coinSymbol: any
   ) => {
     if (!userAddress) {
       throw new Error('Wallet is not connected');
     }
 
+    const tokenAddress = getTokenAddress(coinSymbol);
+
+    if (tokenAddress) {
+      try {
+        const result = await sendTokenTransaction(
+          toAddress,
+          tokenAddress,
+          amount
+        );
+        console.log('Transaction result:', result);
+        return result;
+      } catch (error) {
+        console.error('Failed to send token transaction:', error);
+        throw error;
+      }
+    }
+
+    const transaction = {
+      validUntil: Math.floor(Date.now() / 1000) + 600,
+      messages: [
+        {
+          address: toAddress,
+          amount: amount,
+        },
+      ],
+    };
+
     try {
-      const message = await createJettonTransferMessage(
-        toAddress,
-        amount,
-        collectorAddress
-      );
-
-      const transaction = {
-        validUntil: Math.floor(Date.now() / 1000) + 600,
-        messages: [
-          {
-            address: message.address,
-            amount: '0',
-            payload: message.payload,
-          },
-        ],
-      };
-
-      console.log(transaction);
-
       const result = await tonConnectUI.sendTransaction(transaction);
-      console.log('Jetton transaction result:', result);
+      console.log('Transaction result:', result);
       return result;
     } catch (error) {
-      console.error('Failed to send Jetton transaction:', error);
+      console.error('Transaction failed:', error);
       throw error;
     }
+  };
+
+  const getTokenAddress = (
+    coinSymbol: 'GM' | 'ARBUZ' | 'KINGYTON' | 'DFC' | 'M5'
+  ): string | null => {
+    const tokenAddresses: {
+      [key in 'GM' | 'ARBUZ' | 'KINGYTON' | 'DFC' | 'M5']: string;
+    } = {
+      GM: '0:7d7b64496899d7fed00b0c14b221be688f460724b6bc16772c17ee4c8d7a256d',
+      ARBUZ:
+        '0:0cd8a583a7d94dd18bf1bdf49b234af28c15f033bd2b6a4a4d2076ee1136ad45',
+      KINGYTON:
+        '0:beb5d4638e860ccf7317296e298fde5b35982f4725b0676dc98b1de987b82ebc',
+      DFC: '0:f6eb371de82aa9cfb5b22ca547f31fdc0fa0fbb41ae89ba84a73272ff0bf2157',
+      M5: '0:5ae8ea1f738bd06755d92d361191b5f8a965160427f4b05060a3491dc5d970ea',
+    };
+
+    return tokenAddresses[coinSymbol] || null;
   };
 
   return {
